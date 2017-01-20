93d9aa0fe4e6b453dd749bdf3d939c03f06f1883
Matt Gordon <development@rigidassoftware.co.uk>
2017-01-20 18:35:44 +0000

    Fixed bugs in Base.js

--- test/Config.js
+++ test/Config.js
@@ -1,26 +1,37 @@
 var chai = require('chai'),
     expect = chai.expect,
     should = chai.should(),
-    chaiAsPromised = require("chai-as-promised");
-    Config = require('../lib/Config.js')
-
-chai.use(chaiAsPromised)
+    Config = require('../lib/Config.js');
 
 //--debug-brk" add this to the test script to hit breakpoints
 
 describe('Config', function(){
-    describe('AddNewUserResponse()', function() {
 
-        it("should set properties", function() {
+     describe('AddNewUserRequest', function() {
+
+        it("constructor should set properties", function() {
+            var addNewUserRequest = new Config.AddNewUserRequest("token");
+            expect(addNewUserRequest.AuthorisationToken).to.equal("");
+            expect(addNewUserRequest.LinnworksUniqueIdentifier).to.equal("");
+            expect(addNewUserRequest.Email).to.equal("");
+            expect(addNewUserRequest.AccountName).to.equal("");
+        });
+
+    })
+
+    describe('AddNewUserResponse', function() {
+
+        it("constructor should set properties", function() {
             var addNewUserResponse = new Config.AddNewUserResponse("token");
+            expect(addNewUserResponse.Error).to.equal(null);
             expect(addNewUserResponse.AuthorisationToken).to.equal("token");
         });
 
-    });
+    })
 
-    describe('SaveUserConfigRequest()', function() {
+    describe('SaveUserConfigRequest', function() {
 
-        it("should set default properties", function() {
+        it("constructor should set default properties", function() {
 
             var saveUserConfigRequest = new Config.SaveUserConfigRequest();
 
@@ -28,5 +39,32 @@ describe('Config', function(){
             expect(saveUserConfigRequest.ConfigItems.length).to.equal(0);
         });
 
-    });
-})
+    })
+
+    describe('UserConfigResponse', function() {
+
+        it("constructor should set default properties", function() {
+
+            var configItems = [
+                { 
+                    one: "1"
+                },
+                {
+                    two: "2"
+                }
+            ];
+
+            var userConfigResponse = new Config.UserConfigResponse("step1", "titleAB", "a description", configItems);
+
+            expect(userConfigResponse.Error).to.equal(null);
+            expect(userConfigResponse.StepName).to.equal('step1');
+            expect(userConfigResponse.WizardStepTitle).to.equal('titleAB');
+            expect(userConfigResponse.WizardStepDescription).to.equal('a description');
+            expect(userConfigResponse.ConfigItems).to.equal(configItems);
+
+        });
+
+    })
+
+
+});
